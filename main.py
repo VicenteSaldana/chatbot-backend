fed8a530b81cd9bc35d892075ff96e3346f1f0fd
VicenteSaldana <vicente.saldana@uc.cl>
2024-11-07 10:38:40 -0300

    vercel

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ app.add_middleware(
     allow_headers=["*"],  # Permite todos los encabezados
 )
 app.include_router(router)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# Incluir esta parte solo cuando estás corriendo localmente
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
